27b9f7e3e404fb949c694e0b56d96f28933c7385
JustinSmith77 <152192093+JustinSmith77@users.noreply.github.com>
2024-09-30 08:50:06 -0400

    Add files via upload

---
+++ Java Practice/Temp.java
@@ -0,0 +1,11 @@
+class Temp {
+
+    public static void main(String args[]){
+        double degreesF = 47.5;
+        double degreesC = (5.0/9) * (degreesF - 32);
+
+        System.out.println("The temperature " + degreesF + " degrees F");
+
+        System.out.println("is " + degreesC + " degrees C");
+}
+}
